33ad63749323afeb91d5a05b3070f99d5ec32303
John Sonneville <SonnevilleJ@gmail.com>
2021-08-29 01:02:09 -0500

    Updated logs in character

--- characters/Senax.5043402071080960.js
+++ characters/Senax.5043402071080960.js
@@ -3,10 +3,9 @@ load_code("fight");
 load_code("log");
 
 CurrentLogLevel = LogLevels.Warn;
-logTrace(character.name + " loaded!");
 
 setInterval(main, 250);
-logTrace("Senax main loop started");
+logTrace(character.name + " main loop started");
 
 // Write your own CODE: https://github.com/kaansoral/adventureland
 // character.items.find(i => i.name = "hpot0").q
