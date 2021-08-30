fb21978c74df80609dde8f72483dfa6387ce985b
John Sonneville <SonnevilleJohn@JohnDeere.com>
2021-08-29 22:56:57 -0500

    Add support for Mac

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-cp -R characters /c/Users/Sonne/AppData/Roaming/Adventure\ Land/autosync5337696552091648/adventureland/
-cp -R codes /c/Users/Sonne/AppData/Roaming/Adventure\ Land/autosync5337696552091648/adventureland/
+case "$(uname -s)" in
+  # borrowed from https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux#27776822
+  Darwin)
+    cp -R characters ~/Library/Application\ Support/Adventure\ Land/autosync5337696552091648/adventureland/
+    cp -R codes ~/Library/Application\ Support/Adventure\ Land/autosync5337696552091648/adventureland/
+    ;;
+
+  CYGWIN*|MINGW32*|MSYS*|MINGW*)
+    cp -R characters /c/Users/Sonne/AppData/Roaming/Adventure\ Land/autosync5337696552091648/adventureland/
+    cp -R codes /c/Users/Sonne/AppData/Roaming/Adventure\ Land/autosync5337696552091648/adventureland/
+    ;;
+
+esac
--- load.sh
+++ load.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-cp -R /c/Users/Sonne/AppData/Roaming/Adventure\ Land/autosync5337696552091648/adventureland/* .
+case "$(uname -s)" in
+  # borrowed from https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux#27776822
+  Darwin)
+    cp -R ~/Library/Application\ Support/Adventure\ Land/autosync5337696552091648/adventureland/* .
+    ;;
+
+  CYGWIN*|MINGW32*|MSYS*|MINGW*)
+    cp -R /c/Users/Sonne/AppData/Roaming/Adventure\ Land/autosync5337696552091648/adventureland/* .
+    ;;
+
+esac
